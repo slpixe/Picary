842698cbd7e734ad43530d4fec248720e592a708
Slpixe <slpixe@gmail.com>
2020-11-29 03:07:26 +0000

    re setup cra & some firebase stuff

--- src/App.js
+++ src/App.js
@@ -1,115 +1,72 @@
-import React, { Component } from 'react';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import logo from './logo.svg';
 import './App.css';
-import firebase from './firebase';
-import 'firebase/firestore';
 
-import Button from 'material-ui/Button';
-import Card from 'material-ui/Card';
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAWCvLvmNRXKRiJ6Y0jv4Q_DWQixRlj7pg",
+  authDomain: "picary-b8198.firebaseapp.com",
+  databaseURL: "https://picary-b8198.firebaseio.com",
+  projectId: "picary-b8198",
+  storageBucket: "picary-b8198.appspot.com",
+  messagingSenderId: "481656620124",
+  appId: "1:481656620124:web:7197dd7c503e7c43881299"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-class App extends Component {
-  constructor () {
-    super();
-    firebase.firestore().enablePersistence()
-      .then(() => {
-        let db = firebase.firestore();
-      })
-    this.itemsRef = firebase.firestore().collection('items');
-    this.categoriesRef = firebase.firestore().collection('categories');
-    this.state = {
-      fetching: false,
-      items: [],
-      categories: [],
-      selectedCategory: null
-    }
-  }
+let dbItems = firebase.firestore().collection('items');
+console.log('a');
 
-  componentDidMount () {
-    this.unsubscribeCol = this.itemsRef.onSnapshot(this.onColUpdate);
-    this.unsubscribeCol2 = this.categoriesRef.onSnapshot(this.onCategoriesUpdate);
-    this.setState({fetching: true});
-  }
+// const c = (snapshot) => {
+//   const docs = snapshot.docs.map((docSnapshot) => ({
+//     id: docSnapshot.id,
+//     data: docSnapshot.data()
+//   }));
+//   // this.setState({
+//   //   items: docs,
+//   //   fetching: false
+//   // });
+//   console.log('docs', docs);
+// };
 
-  componentWillUnmount () {
-    this.unsubscribeCol();
-    this.unsubscribeCol2();
-  }
+const getWhereParentNull = () => {
+  console.log('getWhereParentNull');
+  dbItems.where("parent", '==', '')
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(doc =>{
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    })
+    // console.log('qs', querySnapshot);
+  })
+  .catch(function(error) {
+    console.log("Error getting documents: ", error);
+  });
+};
 
-  onColUpdate = (snapshot) => {
-    const docs = snapshot.docs.map((docSnapshot) => ({
-      id: docSnapshot.id,
-      data: docSnapshot.data()
-    }));
-    this.setState({
-      items: docs,
-      fetching: false
-    });
-    console.log('docs', docs);
-  };
-
-  onCategoriesUpdate = (snapshot) => {
-    const docs = snapshot.docs.map((docSnapshot) => ({
-      id: docSnapshot.id,
-      data: docSnapshot.data()
-    }));
-    this.setState({
-      categories: docs,
-      fetching: false
-    });
-    console.log('docs', docs);
-  }
-
-  onSelectCategory = (selectedCategoryItem) => {
-    console.log(selectedCategoryItem.data.name)
-    this.setState({selectedCategory: selectedCategoryItem.data.name})
-  }
-
-  renderCategories = () => {
-    return <div>
-      {this.state.categories.map((item) => {
-        return (
-          <Card key={item.id}>
-            <div onClick={() => this.onSelectCategory(item)}>
-              <div>{`${item.data.label}`}</div>
-              {this.renderSvg(item.data.image)}
-            </div>
-          </Card>
-        )
-      })}
+function App() {
+  console.log('app running');
+  getWhereParentNull();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  }
-
-  filterItems = (items, property, value) => {
-    return items.filter(item => item.data[property] === value)
-  }
-
-  renderitems = () => {
-    if (!this.state.selectedCategory) return
-
-    let filteredItems = this.filterItems(this.state.items, 'category', 'Colors')
-
-    return (
-      <div>
-        {filteredItems.map((item) => <Card
-          key={item.id}><Button>{`${item.data.category} - ${item.data.name}`}</Button></Card>)}
-      </div>
-    )
-  }
-
-  renderSvg = (svg) => {
-    const computedSrc = `data:image/svg+xml;base64,${svg}`
-    return (
-      <img src={computedSrc} />
-    )
-  }
-
-  render () {
-    return (
-      <div className="App">
-        {this.renderCategories()}
-        {this.renderitems()}
-      </div>
-    );
-  }
+  );
 }
 
 export default App;
